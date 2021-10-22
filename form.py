4125af50874411bc3e6b8d691d1c2277c02bc2b2
Zanz2 <zagarzan4@gmail.com>
2021-10-22 14:05:24 +0200

    form validation

--- form.py
+++ form.py
@@ -7,29 +7,46 @@ sg.theme('DarkTeal9')
 excel_file = 'scenarios.xlsx'
 df = pd.read_excel(excel_file)
 
+cities_list = ['City 1', 'City 2', 'City 3', 'City 4', 'City 5', 'City 6', 'City 7']
+city_to_neighbourhoods = {
+    'City 1': ['Neighbourhood 1 in City 1', 'Neighbourhood 2 in City 1', 'Neighbourhood 3 in City 1'],
+    'City 2': ['Neighbourhood 1 in City 2', 'Neighbourhood 2 in City 2', 'Neighbourhood 3 in City 2'],
+    'City 3': ['Neighbourhood 1 in City 3', 'Neighbourhood 2 in City 3', 'Neighbourhood 3 in City 3'],
+    'City 4': ['Neighbourhood 1 in City 4', 'Neighbourhood 2 in City 4', 'Neighbourhood 3 in City 4'],
+    'City 5': ['Neighbourhood 1 in City 5', 'Neighbourhood 2 in City 5', 'Neighbourhood 3 in City 5'],
+    'City 6': ['Neighbourhood 1 in City 6', 'Neighbourhood 2 in City 6', 'Neighbourhood 3 in City 6'],
+    'City 7': ['Neighbourhood 1 in City 7', 'Neighbourhood 2 in City 7', 'Neighbourhood 3 in City 7'],
+}
+
+neighbourhoods_with_train_station = [
+    'Neighbourhood 1 in City 2', 'Neighbourhood 2 in City 2', 'Neighbourhood 3 in City 2',
+    'Neighbourhood 1 in City 5', 'Neighbourhood 2 in City 5', 'Neighbourhood 3 in City 5',
+    'Neighbourhood 1 in City 6', 'Neighbourhood 2 in City 6', 'Neighbourhood 3 in City 6',
+    'Neighbourhood 1 in City 7', 'Neighbourhood 2 in City 7', 'Neighbourhood 3 in City 7'
+]
+
 layout = [
-    [sg.Text('Preffered cuisine or food choices', size=(30, 1)), sg.InputText(key='Enter preffered cuisines or foods')],
-    [sg.Text('Cuisine or food choices to avoid', size=(30, 1)), sg.InputText(key='Enter cuisines or foods to avoid')],
+    [sg.Text('Preffered cuisine or food choices', size=(30, 1)), sg.InputText(key='cuisine_food_pref')],
+    [sg.Text('Cuisine or food choices to avoid', size=(30, 1)), sg.InputText(key='cuisine_food_avoid')],
 
 
-    [sg.Text('Select a restaurant price range', size=(30, 1)), sg.Combo(['Cheap', 'Moderate', 'Expensive'], key='Restaurant price range')],
+    [sg.Text('Select a restaurant price range', size=(30, 1)), sg.Combo(['Cheap', 'Moderate', 'Expensive'], key='restaurant_price_range')],
     # either the above or we make them enter their savings /salary as input text and then we determine whether we find them cheap moderate or expensive restaurants,
     # but they might want cheap restaurants even if they earn little or vice versa
 
-    [sg.Text('Do you want to eat out', size=(30, 1)), sg.Radio('Yes', "RADIO1", default=True),sg.Radio('No', "RADIO1")],
+    [sg.Text('Do you want to eat out', size=(30, 1)), sg.Radio('Yes', "RADIO1",key="eat_out_yes", default=True),sg.Radio('No', "RADIO1",key="eat_out_no")],
     # seems unnecesary, doesnt everyone?
 
-    [sg.Text('Select the city you live in', size=(30, 1)), sg.Combo(['City 1', 'City 2', 'City 3', 'City 4', 'City 5', 'City 6', 'City 7'], key='')],
-    [sg.Text('Select the neighbourhood you live in', size=(30, 1)), sg.Combo(['Neighbourhood from selected city 1', 'Neighbourhood from selected city 2', 'Neighbourhood from selected city 3'], key='')],
-    # TODO: need to add logic so that based on the selected city we find neighbourhoods inside it and present those as options
+    [sg.Text('Select the city you live in', size=(30, 1)), sg.Combo(cities_list, key='select_cities',enable_events=True)],
+    [sg.Text('Select the neighbourhood you live in', size=(30, 1)), sg.Combo(['Please select a city first'], key='select_neighbourhood',enable_events=True)],
 
-    [sg.Text('Select the modes of transport you preffer', size=(30, 1)),sg.Checkbox('Car (gas)'),sg.Checkbox('Car (electric)'), sg.Checkbox('Ride-share'), sg.Checkbox('Train'), sg.Checkbox('Bike')],
-    # preffered transportation, TODO: if neighbourhood doesnt include train station, train should be disabled
 
-    [sg.Text('Select any health conditions you might have', size=(30, 1)), sg.Checkbox('COVID symptoms'),sg.Checkbox('Gluten allergy'), sg.Checkbox('Lactose intolerance')],
+    [sg.Text('Select the modes of transport you preffer', size=(30, 1)),sg.Checkbox('Car (gas)',key="pref_transport_gas_car"), sg.Checkbox('Car (electric)',key="pref_transport_electric_car"), sg.Checkbox('Ride-share',key="pref_transport_rideshare"), sg.Checkbox('Train',key="pref_transport_train"), sg.Checkbox('Bike',key="pref_transport_bike")],
 
-    [sg.Text('Select any additional prefferences', size=(30, 1)),sg.Radio('None', "RADIO2", default=True),sg.Radio('Low Co2 food', "RADIO2"),sg.Radio('Low Co2 transport', "RADIO2")],
-    [sg.Text('Select any additional prefferences', size=(30, 1)), sg.Radio('None', "RADIO3", default=True),sg.Radio('Fast transport', "RADIO3"), sg.Radio('Cheap transport', "RADIO3")],
+    [sg.Text('Select any health conditions you might have', size=(30, 1)), sg.Checkbox('COVID symptoms',key="condition_covid"),sg.Checkbox('Gluten allergy',key="condition_gluten"), sg.Checkbox('Lactose intolerance',key="condition_lactose")],
+
+    [sg.Text('Select any additional prefferences', size=(30, 1)),sg.Radio('None', "RADIO2", default=True,key="pref_co2_none"),sg.Radio('Low Co2 food', "RADIO2",key="pref_co2_low_food"),sg.Radio('Low Co2 transport', "RADIO2",key="pref_co2_low_transport")],
+    [sg.Text('', size=(30, 1)), sg.Radio('None', "RADIO3", default=True,key="pref_transport_none"),sg.Radio('Fast transport', "RADIO3",key="pref_transport_fast"), sg.Radio('Cheap transport', "RADIO3",key="pref_transport_cheap")],
 
     [sg.Submit(), sg.Button('Clear'), sg.Exit()]
 ]
@@ -46,7 +63,29 @@ while True:
         break
     if event == 'Clear':
         clear_input()
+    if event == 'select_cities':
+        city_name = values['select_cities']
+        available_values = city_to_neighbourhoods[city_name]
+        window.Element('select_neighbourhood').update(values=available_values)
+    if event == 'select_neighbourhood':
+        neighbourhood = values['select_neighbourhood']
+        window.Element('pref_transport_train').update(disabled=False)
+        if neighbourhood not in neighbourhoods_with_train_station:
+            window.Element('pref_transport_train').update(value=False,disabled=True)
+
+
     if event == 'Submit':
+        if values['select_neighbourhood'] == 'Please select a city first':
+            sg.popup_ok('Please select a city from the dropdown menu and a valid neighbourhood')
+            continue
+
+        window.Element('select_neighbourhood').update(values=['Please select a city first'])
+        window.Element('pref_co2_none').update(value=True)
+        window.Element('pref_transport_none').update(value=True)
+        window.Element('eat_out_yes').update(value=True)
+        window.Element('pref_transport_train').update(disabled=False)
+
+
         df = df.append(values, ignore_index=True)
         df.to_excel(excel_file, index=False)
         sg.popup('Data saved!')
